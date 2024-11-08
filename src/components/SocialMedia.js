edb59e168a5233cd0225c4fe687b126468de2548
taraknadh <pavan.garsikuti@tftus.com>
2024-11-08 15:48:03 +0530

    added social

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -3,9 +3,9 @@ import "../css/SocialMedia.css";
 function SocialMedia() {
   return (
     <div className='social-media-container'>
-        <a href="https://www.facebook.com/groups/recyclingplastics/" className="fab fa-facebook"></a>
-        <a href="https://x.com/Plastrecynews" className="fab fa-twitter"></a>
-        <a href="https://www.linkedin.com/company/plastic-recycling-limited/?originalSubdomain=uk" className="fab fa-linkedin"></a>
+        <a href="https://www.facebook.com/groups/recyclingplastics/" className="fab fa-facebook">Facebook</a>
+        <a href="https://x.com/Plastrecynews" className="fab fa-twitter">Twitter</a>
+        <a href="https://www.linkedin.com/company/plastic-recycling-limited/?originalSubdomain=uk" className="fab fa-linkedin">LinkedIn</a>
     </div>
   )
 }
